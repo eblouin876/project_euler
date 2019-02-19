83ab642f330622f216431474342d2c304dc649e2
Emile Blouin <emile@D-10-19-123-116.dhcp4.washington.edu>
2019-02-19 13:45:41 -0800

    Now palindrome works with n digits

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,15 @@
 # Find the largest palindromic number resulting from two n-digit numers
 
 # We'll check for two digit numbers First
-palindrome = 11
+palindrome = 0
 
-for x in range(100, 1000):
-    for y in range(100, 1000):
+n = input("Number of digits: ")
+
+for x in range(10**(n-1), 10**n):
+    for y in range(10**(n-1), 10**n):
         total = x * y
         if str(total) == str(total)[::-1]:
             if total > palindrome:
                 palindrome = total
+
 print(palindrome)
